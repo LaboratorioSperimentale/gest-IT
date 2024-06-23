1697e89a0fa5326926ef65471d1163be9c0e6d40
ellepannitto <ellepannitto@gmail.com>
2024-06-23 18:58:37 +0200

    first draft ci script

---
+++ ci/metadata.py
@@ -0,0 +1,19 @@
+import pathlib
+import yaml
+
+_PARTICIPANTS_FOLDER = pathlib.Path("metadata/participants")
+_CONVERSATIONS_FOLDER = pathlib.Path("metadata/conversations")
+
+
+for participant_filename in _PARTICIPANTS_FOLDER.glob("*.yaml"):
+	print(participant_filename)
+	with open(participant_filename) as stream:
+		try:
+			data = yaml.safe_load(stream)
+		except yaml.YAMLError as exc:
+			print(exc)
+			exit()
+
+
+	print(data)
+	input()
